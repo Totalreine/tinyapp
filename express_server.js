9b02559b4220f9d71a7b799321f056eb17b7f3e9
Juan Escobar <jcarriola83@gmail.com>
2023-01-25 06:58:00 +0000

    username displayed

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const { name } = require("ejs");
 const express = require("express");
+var cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -50,16 +52,17 @@ app.post("/urls", (req, res) => {
   });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    const templateVars = { username: req.cookies["username"] }
+    res.render("urls_new", templateVars);
   });
 
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id],  username: req.cookies["username"] };
     res.render("urls_show", templateVars);
   });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = { urls: urlDatabase,  username: req.cookies["username"] };
     res.render("urls_index", templateVars);
   });
 
