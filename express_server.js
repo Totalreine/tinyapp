40d5386c2d8581a68f17857fb9405fc2a9964386
Juan Escobar <jcarriola83@gmail.com>
2023-01-27 06:41:46 +0000

    password hashing and cookies session

--- express_server.js
+++ express_server.js
@@ -1,14 +1,22 @@
-const { name } = require("ejs");
+
 const express = require("express");
-var cookieParser = require('cookie-parser')
+const bcrypt = require("bcryptjs");
+const cookieSession = require('cookie-session')
+const getUserByEmail = require("./helper")
 const { v4: uuidv4 } = require('uuid');
 const app = express();
-app.use(cookieParser())
+
 const PORT = 8080;
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ["supersecret!!"]
+
+}))
 
 const urlDatabase = {
     b6UTxQ: {
@@ -23,29 +31,20 @@ const urlDatabase = {
 
 const users = {
     ID1: {
-      id: "userRandomID",
-      email: "user@example.com",
-      password: "purple-monkey-dinosaur",
+        id: "userRandomID",
+        email: "user@example.com",
+        password: "purple-monkey-dinosaur",
     },
     ID2: {
-      id: "user2RandomID",
-      email: "user2@example.com",
-      password: "dishwasher-funk",
+        id: "user2RandomID",
+        email: "user2@example.com",
+        password: "dishwasher-funk",
     },
-  };
-
-  const getUserByEmail = (email) => {
-    for( let user in users) {
-        if (users[user].email === email) {
-            return email
-        } 
-    }
-    return null
-  }
+};
 
 
 app.get("/register", (req, res) => {
-    let loggedIn = req.cookies["user_id"]
+    let loggedIn = req.session.user_id
     if (!loggedIn) {
         res.render("urls_register")
     } else {
@@ -56,73 +55,76 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     let userEmail = req.body.email
     let userPassword = req.body.password
+    const hashedPassword = bcrypt.hashSync(userPassword, 10);
     let userID = uuidv4()
 
-    if (!userEmail || !userPassword ) {
+    if (!userEmail || !userPassword) {
         return res.status(400).send("Email or password invalid")
     }
-        
-    
-    if (getUserByEmail(userEmail) === userEmail) {
-            return res.status(400).send("Email is already registered")
-        }
-        
-    
+
+
+    if (getUserByEmail(userEmail, users) === userEmail) {
+        return res.status(400).send("Email is already registered")
+    }
+
+
     users[userID] = {
         id: userID,
         email: userEmail,
-        password: userPassword
+        password: hashedPassword
     }
-    
+
     res.redirect("/login")
 })
 
 app.post("/logout", (req, res) => {
-    res.clearCookie("user_id")
+    req.session = null
     res.redirect("/login")
 })
 
 app.get("/login", (req, res) => {
-    let loggedIn = req.cookies["user_id"]
+    let loggedIn = req.session.user_id
     if (!loggedIn) {
         res.render("urls_login.ejs")
     } else {
         res.redirect("/urls")
     }
-    
+
 })
 
 app.post("/login", (req, res) => {
     let userEmail = req.body.email
     let userPassword = req.body.password
+    let hashedPassword
     let userID
-    for(let user in users) {
-        if(users[user].email === userEmail) {
+    for (let user in users) {
+        if (users[user].email === userEmail) {
             userID = user
+            hashedPassword = users[user].password
         }
     }
 
-    if (!getUserByEmail(userEmail)) {
+    if (!getUserByEmail(userEmail, users)) {
         return res.status(403).send("Email not registered")
     }
-    if ( users[userID].password !== userPassword) {
+    if (!bcrypt.compareSync(userPassword, hashedPassword)) {
         return res.status(403).send("Incorrect password")
     }
 
-    res.cookie("user_id", userID )
+    req.session.user_id = userID;
     res.redirect("/urls")
 })
 
-//EDIT
+
 app.post("/urls/:id", (req, res) => {
-    let userID = req.cookies["user_id"]
+    let userID = req.session.user_id
     let id = req.params.id
     let newURL = req.body.url
-    if (urlDatabase[req.params.id] === undefined) {
+    if (urlDatabase[id] === undefined) {
         res.render("urls_wrongId")
     }
 
-    if (urlDatabase[req.params.id].userID !== userID) {
+    if (urlDatabase[id].userID !== userID) {
         res.render("urls_notBelongToID")
     }
 
@@ -133,18 +135,18 @@ app.post("/urls/:id", (req, res) => {
         res.redirect("/urls")
     }
 
-   
-})
-//DELETE
-app.post("/urls/:id/delete", (req, res) => {
-    let userID = req.cookies["user_id"]
-    const id = req.params.id
 
-    if (urlDatabase[req.params.id] === undefined) {
+})
+
+app.post("/urls/:id/delete", (req, res) => {
+    let userID = req.session.user_id
+    let id = req.params.id
+
+    if (urlDatabase[id] === undefined) {
         res.render("urls_wrongId")
     }
 
-    if (urlDatabase[req.params.id].userID !== userID) {
+    if (urlDatabase[id].userID !== userID) {
         res.render("urls_notBelongToID")
     }
 
@@ -155,93 +157,97 @@ app.post("/urls/:id/delete", (req, res) => {
         res.redirect("/urls")
     }
 
-   
+
 })
 
 app.get("/u/:id", (req, res) => {
-    const longURL = urlDatabase[req.params.id].longURL
-    if(longURL === undefined) {
+    const id = urlDatabase[req.params.id]
+    if (id === undefined) {
         res.render("urls_wrongId")
     } else {
-        res.redirect(longURL);
+        res.redirect(id.longURL);
     }
-    
-  });
+
+});
 
 app.post("/urls", (req, res) => {
     let id = (Math.random() + 1).toString(36).substring(6);
-    let loggedIn = req.cookies["user_id"] 
-    if(!loggedIn) {
+    let loggedIn = req.session.user_id
+    if (!loggedIn) {
         res.render("urls_forbidden")
     } else {
         urlDatabase[id] = {
             longURL: req.body.longURL,
             userID: loggedIn
         }
-        res.redirect(`/urls/${id}`); 
-        
+        res.redirect(`/urls/${id}`);
+
     }
-    
-  });
+
+});
 
 app.get("/urls/new", (req, res) => {
-    let id = req.cookies["user_id"]
+    console.log(users)
+    let id = req.session.user_id
     const templateVars = { user: users[id] }
-    if(!id) {
+    if (!id) {
         res.redirect("/login")
     } else {
         res.render("urls_new", templateVars);
     }
-        
-  });
+
+});
 
 app.get("/urls/:id", (req, res) => {
-    let id = req.cookies["user_id"]
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL,  user: users[id] };
-    if(!id) {
+    console.log(urlDatabase)
+    let userID = req.session.user_id
+    let id = req.params.id
+    const templateVars = { id: id, url: urlDatabase[id],  user: users[userID] };
+    if (!userID) {
         res.render("urls_notLoggedIn")
-    } else if(urlDatabase[req.params.id].userID !== id ) {
+    } else if (urlDatabase[id] === undefined) {
+        res.render("urls_wrongId")
+    } else if (urlDatabase[id].userID !== userID) {
         res.render("urls_notBelongToID")
-    }
-    else {
+    } else {
         res.render("urls_show", templateVars);
     }
-    
-  });
 
-  const urlsForUser = (id) => {
-   let newDB = {}
-   for(let key in urlDatabase) {
-    if(urlDatabase[key].userID === id) {
-        newDB[key] = urlDatabase[key]
+});
+
+const urlsForUser = (id) => {
+    let userURLS = {}
+    for (let key in urlDatabase) {
+        if (urlDatabase[key].userID === id) {
+            userURLS[key] = urlDatabase[key]
+        }
+
     }
+    return userURLS
+}
 
-   }
-   return newDB
-  }
-    
-    
+
 
 app.get("/urls", (req, res) => {
-    let id = req.cookies["user_id"]
-    let filteredDatabe = urlsForUser(id)
-    const templateVars = { urls: filteredDatabe,  user: users[id] };
-    if(!id) {
+    let userID = req.session.user_id
+    let filteredDatabe = urlsForUser(userID)
+    const templateVars = { urls: filteredDatabe,  user: users[userID] };
+    if (!userID) {
         res.render("urls_notLoggedIn")
     } else {
         res.render("urls_index", templateVars);
     }
-    
-  });
+
+});
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
-  });
-  
+});
+
 app.get("/", (req, res) => {
-  res.redirect("/register");
+    res.redirect("/register");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+    console.log(`Example app listening on port ${PORT}!`);
 });
